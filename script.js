d980a00f55caee92561cba6af85c2d29134679d7
Robodlan <reinaldo.robles@icloud.com>
2024-02-23 11:41:43 -0400

    script .js functions

--- script.js
+++ script.js
@@ -17,6 +17,25 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const weapons = [
+    {
+        name: "stick",
+        power: 5,
+    },
+    {
+        name: "dagger",
+        power: 30,
+    },
+    {
+        name: "claw hammer",
+        power: 50,
+    },
+    {
+        name: "sword",
+        power: 100,
+    }
+];
+
 const locations = [
     {
         name: "town square",
@@ -73,12 +92,22 @@ function fightDragon() {
 };
 
 function buyHealth() {
-    gold -= 10;
-    health += 10;
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health
+    } else {
+        text.innerText = "You do not have enough gold to buy health."
+    }
 };
 
 function buyWeapon() {
- 
+    if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold
+    }
 };
 
 function fightSlime() {
